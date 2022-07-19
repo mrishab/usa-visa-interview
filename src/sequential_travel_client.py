11a549bd2459d84f150a7a604991898845627399
Rishab Manocha <rishab.manocha@outlook.com>
2022-07-18 21:23:38 -0700

    Added exception handling for multi-city search

--- src/sequential_travel_client.py
+++ src/sequential_travel_client.py
@@ -8,5 +8,10 @@ class SequentialTravelClient:
         self.travel_client = travel_client
 
     def get_visa_wait_times(self, get_visa_wait_times_req: GetVisaWaitTimesRequest) -> GetVisaWaitTimesResponse:
-        wait_times = [self.travel_client.get_visa_wait_time(get_visa_wait_time_req) for get_visa_wait_time_req in get_visa_wait_times_req.get_all()]
+        wait_times = []
+        for get_visa_wait_time_req in get_visa_wait_times_req.get_all():
+            try:
+                wait_times.append(self.travel_client.get_visa_wait_time(get_visa_wait_time_req))
+            except:
+                pass
         return GetVisaWaitTimesResponse(wait_times=wait_times)
